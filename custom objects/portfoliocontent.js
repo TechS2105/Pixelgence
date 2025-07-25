ada18b946c43bde922a0cfee9ea4f2e08e5d0600
TechS2105 <techsovangithub0521@gmail.com>
2025-07-26 00:17:59 +0530

    add portfoliocontent.js and update Portfoliosection.module.css and Portfoliosection.jsx file code

---
+++ custom objects/portfoliocontent.js
@@ -0,0 +1,37 @@
+const portfoliocontent = [
+
+    {
+
+        id: 1,
+        image: "https://images.pexels.com/photos/7970815/pexels-photo-7970815.jpeg",
+        title: "Digital Strategy"
+
+    },
+
+    {
+
+        id: 2, 
+        image: "https://images.pexels.com/photos/4348401/pexels-photo-4348401.jpeg",
+        title: "Digital Design"
+
+    },
+
+    {
+
+        id: 3,
+        image: "https://images.pexels.com/photos/6248957/pexels-photo-6248957.jpeg",
+        title: "Digital Data"
+
+    },
+
+    {
+
+        id: 4,
+        image: "https://images.pexels.com/photos/6476367/pexels-photo-6476367.jpeg",
+        title: "Digital Marketing"
+
+    }
+
+]
+
+export default portfoliocontent;
--- src/components/Portfoliosection.jsx
+++ src/components/Portfoliosection.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
-
+import PortfolioSectionStyle from '../../public/styles/Portfoliosection.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+import { NavLink } from 'react-router-dom';
+import PortfolioContent from '../../custom objects/portfoliocontent.js';
 
 function Portfoliosection() {
     
     return (
 
-        <h1> Hello </h1>
+        <>
+        
+            
+            
+            <div className={PortfolioSectionStyle.portfolioDiv}>
+
+                <div className={PortfolioSectionStyle.portfolioContentDiv}>
+
+                    <div className={PortfolioSectionStyle.contentSection}>
+
+                        <span> Portfolio </span>
+                        <h2>Inovation and Digital Branding</h2>
+
+                    </div>
+
+                </div>
+
+                <div className={PortfolioSectionStyle.portfoliosSection}>
+
+                    <Swiper className={PortfolioSectionStyle.portfolioBoxes}
+                    
+                        slidesPerView={2.5}
+                        spaceBetween={20}
+                        loop={true}
+                        speed={1500}
+                        autoplay={{
+                            
+                            delay: 2500
+
+                        }}
+
+                        modules={[Autoplay]}
+                        
+                    >
+
+                        {PortfolioContent.map((item) => {
+
+                            return (
+
+                                <SwiperSlide className={PortfolioSectionStyle.portfolioBox} key={item.id}>
+
+                                    <div className={PortfolioSectionStyle.portfolioBoxImage}>
+                                        
+                                        <img src={item.image} alt={item.title} />
+
+                                    </div>
+
+                                    <div className={PortfolioSectionStyle.portfolioImgBoxTitle}>
+
+                                        <h3><NavLink to="#"> {item.title} </NavLink></h3>
+
+                                    </div>
+
+                                </SwiperSlide>
+
+                            );
+
+                        })}
+                        
+
+                    </Swiper>
+
+                </div>
+
+            </div>
+            
+        </>
 
     );
 
